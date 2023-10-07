a675f05275df37022c3776dfd7ce496c3ecae86a
ElPiod <malickelhadjo07@gamil.com>
2023-10-07 22:09:45 +0000

    quiz

--- question.js
+++ question.js
@@ -4,68 +4,112 @@ let questions = [
 
     {
         numb: 1,
-        question: "Qui le 8e Hokage dans Boruto blue Vortex ?",
-        answer: "Shikamaru",
+        question: "Quel est le 1er homme de l'equipage de God Roger ?",
+        answer: "Silver Rayleigh",
         options: [
-            "Naruto",
-            "Konohamru",
-            "Shikamaru",
-            "Sakura"    
+            "Baggy le Clown",
+            "Shanks le roux",
+            "Silver Rayleigh",
+            "Crocus"    
         ]
     },
     {
         numb: 2,
-        question: "Qui a emprisonne Naruto ?",
-        answer: "Kawaki",
+        question: "Quel est le fruit du demon de Ace ?",
+        answer: "Pyro-Fruit",
         options: [
-            "Kawaki",
-            "Ishiki",
-            "Momoshiki",
-            "Kaguya"    
+            "Gum Gum",
+            "Pyro-fire",
+            "Pyro-Fruit",
+            "Pyro-demon"    
         ]
     },
     {
         numb: 3,
-        question: "Qui est le chef des Sevens Dealy Sins ?",
-        answer: "Maillodas",
+        question: "Barbe Blanche a le pouvoir de :",
+        answer: "Fissurant",
         options: [
-            "King",
-            "Escanor",
-            "Maillodas",
-            "Arthur"    
+            "Changer le monde",
+            "Engloutire le monde",
+            "Detruire le monde",
+            "Fissurant"    
         ]
     },
     {
         numb: 4,
-        question: "Comment s'appelle le demon de Asta ?",
-        answer: "Lieme",
+        question: "Quel est la mort la plus douloureuse pour luffy ?",
+        answer: "Vogue Merry",
         options: [
-            "Divil",
-            "Lucifer",
-            "Demon",
-            "Lieme"    
+            "Shanks",
+            "Sabot",
+            "Ace",
+            "Vogue Merry"    
         ]
     },
     {
         numb: 5,
-        question: "Dans One piece qui a tue Ace ?",
-        answer: "Amiral Akainu",
+        question: "Qui est le pere de Ace ?",
+        answer: "Gold Roger",
         options: [
-            "Shanks",
-            "Big Mum",
-            "Amiral Akainu",
-            "Kaido"    
+            "Barbe Blanche",
+            "Monkey-D Garp",
+            "Gold Roger",
+            "Sengoku"    
         ]
     },
     {
         numb: 6,
-        question: "Qui a tue Kaido?",
-        answer: "Luffy",
+        question: "Zoro a combien de sabre dans l'aventure?",
+        answer: "3",
+        options: [
+            "4",
+            "3",
+            "6",
+            "5"    
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qui a capturer Ace pour etre grand corsaire ?",
+        answer: "Marshall D teach",
+        options: [
+            "Barbe Blanche",
+            "Doc Q",
+            "Van auger",
+            "Marshall D teach"    
+        ]
+    },
+    {
+        numb: 8,
+        question: "Quel est le 1er ami que se fait luffy au debut son aventure ?",
+        answer: "Zoro",
         options: [
             "Shanks",
-            "Big Mum",
-            "Goku",
-            "Luffy"    
+            "Zoro",
+            "Kobby",
+            "hermep"    
+        ]
+    },
+     {
+        numb: 9,
+        question: "Quel est l' ennemie du Zoro?",
+        answer: "Bartholomew kuma",
+        options: [
+            "Baggy",
+            "Creek",
+            "Arbyda",
+            "Bartholomew kuma"    
+        ]
+    },
+    {
+        numb: 10,
+        question: "Quel est le sabre le plus puissance de Zoro?",
+        answer: "Shusui",
+        options: [
+            "Kitesu",
+            "Shusui",
+            "Bankai",
+            "Le 2e Kitesu"    
         ]
     },
 
